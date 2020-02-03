fca1f41f2d0faf5aee18fe20cf58cdc592673321
Kanakaraju Gandreddi <Kanakaraju.Gandreddi@ril.com>
2020-02-03 17:12:59 +0530

    first commit

---
+++ IOSPipelineEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSPipelineEx
+//
+//  Created by Kanakaraju Gandreddi on 03/02/20.
+//  Copyright © 2020 Kanakaraju Gandreddi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOSPipelineExTests/IOSPipelineExTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOSPipelineExTests.swift
+//  IOSPipelineExTests
+//
+//  Created by Kanakaraju Gandreddi on 03/02/20.
+//  Copyright © 2020 Kanakaraju Gandreddi. All rights reserved.
+//
+
+import XCTest
+@testable import IOSPipelineEx
+
+class IOSPipelineExTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
